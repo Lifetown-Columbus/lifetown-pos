ff5635bae0cc4b4b9aec23de0e54d2473a54f374
Stephen Turley <turley714@gmail.com>
2018-10-27 10:44:36 -0400

    added android device test step

--- app/src/androidTest/java/org/lifetowncolumbus/pos/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/lifetowncolumbus/pos/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("org.lifetowncolumbus.lifetownpos", appContext.packageName)
+        assertEquals("org.lifetowncolumbus.pos", appContext.packageName)
     }
 }
