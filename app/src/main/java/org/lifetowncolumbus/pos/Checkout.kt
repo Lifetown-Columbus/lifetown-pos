651a823c8f680be4e007d99797121f57f2dcc51c
Stephen Turley <turley714@gmail.com>
2018-10-28 19:35:34 -0400

    added Total and AddItem button

--- app/src/main/java/org/lifetowncolumbus/pos/Checkout.kt
+++ app/src/main/java/org/lifetowncolumbus/pos/Checkout.kt
@@ -2,11 +2,25 @@ package org.lifetowncolumbus.pos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.text.NumberFormat
 
 class Checkout : AppCompatActivity() {
+    var total = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+    }
+
+    fun launchAddItemDialog(view: View) {
+        //TODO: use a view model for this
+        total += 1.0
+        val format = NumberFormat.getCurrencyInstance()
+        findViewById<TextView>(R.id.total).apply {
+            text = format.format(total)
+        }
+
     }
 }
