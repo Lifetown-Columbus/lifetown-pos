7c5c02f41a299216aea8b5d0a535bed8247a72df
Stephen Turley <turley714@gmail.com>
2018-12-02 17:49:13 -0500

    added UI tests

---
+++ app/src/androidTest/java/org/lifetowncolumbus/pos/CheckoutInstrumentedTest.kt
@@ -0,0 +1,34 @@
+package org.lifetowncolumbus.pos
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.runner.RunWith
+
+import org.junit.Rule
+import org.junit.Test
+import org.lifetowncolumbus.pos.activities.Checkout
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class CheckoutInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<Checkout> = ActivityTestRule(Checkout::class.java)
+
+    @Test
+    fun testAddItem_computeTotal() {
+        onView(withId(R.id.itemValue))
+            .perform(typeText("5"), closeSoftKeyboard())
+
+        onView(withId(R.id.addItemButton)).perform(click())
+
+        onView(withId(R.id.total)).check(matches(withText("$5.00")))
+    }
+}
