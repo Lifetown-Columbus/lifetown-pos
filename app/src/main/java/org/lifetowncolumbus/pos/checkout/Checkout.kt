eed92609b6af67aa4f3b02df4f553065bb15a0ab
Stephen Turley <turley714@gmail.com>
2018-12-31 17:25:22 -0500

    hid mutableLiveData in the Checkout ViewModel

--- app/src/main/java/org/lifetowncolumbus/pos/checkout/Checkout.kt
+++ app/src/main/java/org/lifetowncolumbus/pos/checkout/Checkout.kt
@@ -1,23 +1,25 @@
 package org.lifetowncolumbus.pos.checkout
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import java.math.BigDecimal
 
 class Checkout : ViewModel() {
-    val items: MutableLiveData<ArrayList<Item>> by lazy {
+    private val mItems: MutableLiveData<ArrayList<Item>> by lazy {
         MutableLiveData<ArrayList<Item>>().apply {
             value = ArrayList()
         }
     }
+    val items: LiveData<ArrayList<Item>> = mItems
 
     val total: BigDecimal
         get() = items.value!!.map { it.value }.fold(BigDecimal.ZERO, BigDecimal::add)
 
     fun addItem(item: Item) {
-        items.value?.add(item)
+        mItems.value?.add(item)
         //force onChange to fire
-        items.value = items.value
+        mItems.postValue(mItems.value)
     }
 
     fun payCash(payment: CashPayment) {
