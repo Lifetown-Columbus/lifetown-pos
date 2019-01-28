7462f1fd57e0698dbcded2378d087792eb45be49
Stephen Turley <turley714@gmail.com>
2019-01-27 19:35:41 -0500

    added catalog view to the edit catalog screen

--- app/src/androidTest/java/org/lifetowncolumbus/pos/merchant/CatalogInstrumentedTest.kt
+++ app/src/androidTest/java/org/lifetowncolumbus/pos/merchant/CatalogInstrumentedTest.kt
@@ -1,18 +1,18 @@
 package org.lifetowncolumbus.pos.merchant
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.lifetowncolumbus.pos.R
+import org.lifetowncolumbus.pos.merchant.models.CatalogItem
 
 class CatalogInstrumentedTest : TestHarness() {
 
@@ -28,5 +28,22 @@ class CatalogInstrumentedTest : TestHarness() {
 
         onView(withId(R.id.saveCatalogItemButton)).perform(click())
         onView(withId(R.id.addSaleItem)).check(matches(withText("Flerp")))
+    }
+
+    @Test
+    fun editCatalog_editCatalogItem() {
+        catalogItemDao.insert(CatalogItem(null, "Widget", 500.00))
+        onView(withId(R.id.editCatalogButton)).perform(click())
+
+        onView(withId(R.id.editCatalogRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.catalogItemName)).perform(typeText("NewWidget"))
+        onView(withId(R.id.catalogItemValue)).perform(typeText("99"))
+
+        onView(withId(R.id.saveCatalogItemButton)).perform(click())
+        onView(withId(R.id.addSaleItem)).check(matches(withText("NewWidget")))
+
+
     }
 }
--- app/src/main/java/org/lifetowncolumbus/pos/merchant/views/catalog/EditCatalogFragment.kt
+++ app/src/main/java/org/lifetowncolumbus/pos/merchant/views/catalog/EditCatalogFragment.kt
@@ -7,25 +7,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_edit_catalog.*
 import kotlinx.android.synthetic.main.fragment_edit_catalog.view.*
 import org.lifetowncolumbus.pos.R
+import org.lifetowncolumbus.pos.merchant.viewModels.Catalog
 
 
 class EditCatalogFragment : androidx.fragment.app.Fragment() {
 
     private lateinit var addCatalogItem : Button
+    private lateinit var catalog: Catalog
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addCatalogItem = view.addCatalogItemButton
         addCatalogItem.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_editCatalogFragment_to_addCatalogItemFragment)
         }
+
+        initEditCatalogView(view)
     }
+
+    private fun initEditCatalogView(view: View) {
+        val recyclerView = view.editCatalogRecyclerView
+        val adapter = CatalogGridAdapter(this.activity!!) {
+           //click handler
+        }
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = androidx.recyclerview.widget.GridLayoutManager(this.activity, 6)
+        catalog.allItems.observe(this, Observer { items ->
+            items?.let { adapter.setItems(it) }
+        })
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_edit_catalog, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.run {
+            catalog = ViewModelProviders.of(this).get(Catalog::class.java)
+        } ?: throw Exception("Invalid Activity")
     }
 }
