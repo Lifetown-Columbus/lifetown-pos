2dac13bb50ad28e9ad98fab7853e5eac0a42d064
Stephen Turley <turley714@gmail.com>
2019-02-05 20:04:36 -0500

    added toolbar to top of screen

--- app/src/main/java/org/lifetowncolumbus/pos/merchant/POSActivity.kt
+++ app/src/main/java/org/lifetowncolumbus/pos/merchant/POSActivity.kt
@@ -1,9 +1,12 @@
 package org.lifetowncolumbus.pos.merchant
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import org.lifetowncolumbus.pos.R
 
 class POSActivity : AppCompatActivity() {
@@ -13,6 +16,10 @@ class POSActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pos)
 
         supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        NavigationUI.setupWithNavController(toolbar, navController)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
     }
