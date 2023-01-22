6f2d78bea6b83e3081283fdaaf4a73329147aece
Stephen Turley <turley714@gmail.com>
2023-01-22 12:56:20 -0500

    upgraded gradle to 7.4

--- app/src/main/java/org/lifetowncolumbus/pos/ConfigFragment.kt
+++ app/src/main/java/org/lifetowncolumbus/pos/ConfigFragment.kt
@@ -1,13 +1,10 @@
 package org.lifetowncolumbus.pos
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_config.view.*
 
